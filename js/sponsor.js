43c7ab6ffb6f8822590d41439eb8ec329a6255bc
Smit Dhameliya <smitdhameliya10@gmail.com>
2019-08-26 21:15:03 +0530

    9:14PM

---
+++ js/sponsor.js
@@ -0,0 +1,22 @@
+ $(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1200,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 1
+            }
+        }]
+    });
+});
